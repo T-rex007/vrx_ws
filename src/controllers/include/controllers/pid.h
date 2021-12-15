a45e980f6a50c4de539d694be290cfe76398b2f5
Kevin Seegobin <58779649+KevinSeegobin@users.noreply.github.com>
2021-12-15 15:43:16 -0400

    Controllers (#3)
    * Added controllers package
    
    * Added MIT license
    
    * Added c compiler and pid cpp
    
    * Fixed CMakeLists
    
    * created cpp and header files
    
    * created cpp and header files
    
    * attempt 2 to fix inconsistancy
    
    * Updated gitignore
    
    * attempt 3 to fix inconsistancy
    
    * firt attempt at PID controller code
    
    * add functions to header file
    
    * Fixed Time issue
    
    * Fixed gitignore
    
    * Fixed Seegobin Nonesense Code
    
    Co-authored-by: Kevin Sankar <dev@kmsankar.com>
    Co-authored-by: Kevin Seegobin <kevinaseegobin@gmail.com.com>

--- src/controllers/include/controllers/pid.h
+++ src/controllers/include/controllers/pid.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
+#include <time.h>
 
 class PID
 {
@@ -13,8 +14,26 @@ public:
     /// @brief Destructor for the PID
     ~PID();
 
+    /// @brief Compute output
+    float Compute(float input);
+
+    ///@brief Set the new reference of the PID
+    void SetRef(float ref);
+
+    ///@brief manually set the gains of the PID controller
+    void SetGains(float p, float i, float d);
+
+    ///@brief tune PID to automatically set gains
+    void TuneGains();
+
 private:
     ros::NodeHandle node; // ROS node handler
+    float setpoint;
+    float kp;
+    float ki;
+    float kd;
+    float last_error;
+    ros::Time last_time;
 };
 
 #endif
--- src/controllers/src/pid.cpp
+++ src/controllers/src/pid.cpp
@@ -12,3 +12,53 @@ PID::~PID()
 {
     return;
 }
+
+/// @brief Compute output
+float PID::Compute(float input)
+{
+    ros::Time now;
+    double difference;
+    double error_p;
+    double error_i;
+    double error_d;
+    float output;
+
+    now = ros::Time::now();
+    difference = (now - last_time).toSec();
+    
+    error_p = setpoint - input;
+    error_i += (error_p * difference);
+    error_d = (error_p - last_error) / difference;
+
+    output = (kp * error_p) + (ki * error_i) + (kd * error_d);
+
+    last_time = now;
+    last_error = error_p;
+
+    return output;
+}
+
+///@brief Set the new reference of the PID
+void PID::SetRef(float ref)
+{
+    setpoint = ref;
+}
+
+///@brief manually set the gains of the PID controller
+void PID::SetGains(float p, float i, float d)
+{
+    kp = p;
+    ki = i;
+    kd = d;
+}
+
+///@brief tune PID to automatically set gains
+void PID::TuneGains()
+{
+    float p;
+    float i;
+    float d;
+
+    // SetGains(p, i, d);
+}
+
