48942342ebdc65cf4f66f633482bb25c2f26c881
Kevin Seegobin <kevinaseegobin@gmail.com>
2021-12-15 04:04:39 -0400

    First try for update functions (goal, location, angle and target vector)

--- src/navigation/include/navigation/wamv.h
+++ src/navigation/include/navigation/wamv.h
@@ -3,6 +3,7 @@
 
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
+#include <math.h>
 
 class WAMV
 {
@@ -13,8 +14,22 @@ public:
     /// @brief Destructor for the WAMV
     ~WAMV();
 
+    ///@brief Update wamv location (x, y) and heading (in degrees)
+    void UpdateLocal(float local[2], float theta);
+
+    ///@brief Update the goal position (x, y)
+    void UpdateGoal(float aim[2]);
+
+    ///@brief Update the target vector and reference angle to goal
+    void UpdateAngle();
+
 private:
     ros::NodeHandle node; // ROS node handler
+    float location[2];   //x and y pos of robot center
+    float heading;    //heading of front of the robot 
+    float goal[2];  //location of goal position
+    float target_vector[2];     //vector from current location to the goal
+    float angle;        //angle between heading and goal distance
 };
 
 #endif
