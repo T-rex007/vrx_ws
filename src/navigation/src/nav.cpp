bc6ea41c2cdd5e6bf2728b7edbaae88a7d54f621
Kevin Seegobin <kevinaseegobin@gmail.com>
2021-12-15 15:48:11 -0400

    start navigation code

--- src/navigation/src/nav.cpp
+++ src/navigation/src/nav.cpp
@@ -1,5 +1,21 @@
 #include <ros/ros.h>
 #include "navigation/wamv.h"
+#include "controllers/pid.h"
+
+#define SAMPLE 100
+
+WAMV boat;
+PID controller;
+
+void GPSCallback(const std::msgs::Float::ConstPtr& msg)
+{
+    boat.UpdateLocal(msg.longitude, msg.latitude)
+}
+
+void GoalCallback(const std::msgs::Float::ConstPtr& msg)
+{
+    boat.UpdateGoal(msg.longitude, msg.latitude)
+}
 
 int main(int argc, char **argv)
 {
@@ -8,6 +24,15 @@ int main(int argc, char **argv)
 
     // Main access point to communications with the ROS system
     ros::NodeHandle n;
+    ros::Subscriber gps = n.subscribe("chatter", 100, GPSCallback);
+    ros::Subscriber goal = n.subscribe("chatter", 100, GoalCallback);
+
+    
+
+    while ros.ok()
+    {
+
+    }
 
     // Wait for this node to be shutdown, whether through Ctrl-C, ros::shutdown() or similar
     ros::waitForShutdown();
